14218d7d8fdcbc37f319b153ac7e320988f1ef31
Mehryar Sadati <mehryarsadati@gmail.com>
2025-02-21 22:23:43 +0330

    pathetic try in cleaning the data

--- main.py
+++ main.py
@@ -30,9 +30,9 @@ word = input("word: ")
 first_word = data["Line"]
 matching_line = data[first_word.str.contains(f"{word} ", na=False, case=False)]["Line"]
 
-# for line in matching_line:
-#     new_line = str(line)
-#     list_of_lines = new_line.split(" ")
-#     print (list_of_lines, "\n")
+for line in matching_line:
+    new_line = str(line)
+    list_of_lines = new_line.split(";")
+    print (list_of_lines, "\n")
 
 print(matching_line)
