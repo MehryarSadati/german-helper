755dd8555ea4bc747c958688eecdb859cf0a986b
Mehryar Sadati <mehryarsadati@gmail.com>
2025-02-22 17:23:12 +0330

    adding words function added

--- artikeln.py
+++ artikeln.py
@@ -1,6 +1,9 @@
 import pandas as pd
+import csv
 
-data = pd.read_csv("artikeln.csv", names=["Line"])
+file_path = "artikeln.csv"
+df = pd.read_csv(file_path)
+data = pd.read_csv(file_path, names=["Line"])
 word = input("What is the word you're looking for?\n>")
 
 matching_line = str(data[data["Line"].str.contains(rf"\b{word}\b", na=False, case=False)]["Line"])
@@ -17,3 +20,12 @@ print(artikel_dict[artikel])
 #this is so that I wouldn't forget to fix this problem.
 if artikel == "die":
     print("this part needs to be upgrated and currently the plurals and femenines aren't seperated.")
+
+def add_word(file_path, new_row):
+    with open(file_path, 'a', newline='\n') as file:
+        writer = csv.writer(file)
+        writer.writerow(new_row)
+
+last_number = df.iloc[-1, 0] 
+new_row = [last_number + 1, "das", "Sakko"]
+add_word(file_path, new_row)
