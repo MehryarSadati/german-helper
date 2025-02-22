076c35e32195fb0748d4d823e3c6090e59af0671
Mehryar Sadati <mehryarsadati@gmail.com>
2025-02-22 16:00:53 +0330

    this is reminder for me to write the web scraping version of this program

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
 """
-this file is not written yet and it's designed for a web scraping version that does the exact same thing.
+this file is not written yet and it's here to remind me to write the web scraping version of this as well.
+that version would most likely contain all the nouns in german language.
 """
